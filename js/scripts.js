db824fcd94e7c68e46f24365b4d6abc11a2283ce
averyzab <71524247+averyzab@users.noreply.github.com>
2020-10-09 11:28:36 -0400

    responsive, bullet edits

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Welcome to my Portfolio!");
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "navtoggle";
   }
