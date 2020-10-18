7c21ffd44b92f40ca6f844960e3a9876e57a7076
averyzab <71524247+averyzab@users.noreply.github.com>
2020-10-18 16:28:13 -0400

    replaced placeholder images

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 console.log("Welcome to my Portfolio!");
+
 function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
-  } else {
-    x.className = "navtoggle";
-  }
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+        x.className += " responsive";
+    } else {
+        x.className = "navtoggle";
+    }
 }
